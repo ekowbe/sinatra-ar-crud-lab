1a9f009c53339c8c7d234c1bcdc6e3f4cea73d73
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-01 13:10:27 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,54 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    @id = Article.last.id + 1
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @id = params[:id]
+    @article = Article.find(@id)
+
+    erb :show
+  end
+
+  post '/articles/:id' do
+    @title = params[:title]
+    @content = params[:content]
+    @article = Article.create(title: @title, content: @content)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @id = params[:id]
+    @article = Article.find(@id)
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @title = params[:title]
+    @content = params[:content]
+    @article = Article.find(params[:id])
+    @article = Article.update(title: @title, content: @content)
+    
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    @id = params[:id]
+    Article.destroy(@id)
+    @articles = Article.all
+
+    erb :index
+  end
 end
